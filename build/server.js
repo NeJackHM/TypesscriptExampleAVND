b1640c4ccbb832b2f5e1b5943c4336aeb94bc951
Nelson Jack Huaman Mendez <n.jack.huaman.mendez@avanade.com>
2020-11-23 03:42:55 -0300

    commit3

--- build/server.js
+++ build/server.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 class Server {
     constructor() {
         this.app = express_1.default();
@@ -19,7 +20,8 @@ class Server {
         this.app.use(helmet_1.default());
     }
     routes() {
-        this.app.get('/', (req, res) => res.send('hello'));
+        // this.app.get('/',(req,res)=>res.send('hello'))
+        this.app.use(indexRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import mogoose from 'mongoose'
+
+import indexRoutes from './routes/indexRoutes';
 
 
 class Server {
@@ -13,15 +16,19 @@ class Server {
     }
 
     config(){
+        const MONGO_URI =''
+        //settings
         // this.app.set('port',process.env.PORT || 3000);
         this.app.set('port',3000);
+        //middlewares
         this.app.use(morgan('dev'));
         this.app.use(helmet());
 
     }
 
     routes(){
-        this.app.get('/',(req,res)=>res.send('hello'))
+        // this.app.get('/',(req,res)=>res.send('hello'))
+        this.app.use(indexRoutes);
 
     }
     start(){
